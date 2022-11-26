0c96eacb00917d6433a8b6ba25540359da08ecec
Afraaz <ahmedalif102@gmail.com>
2022-11-26 12:26:36 +0600

    category function

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const cors = require('cors');
-const app = express()
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const dotenv = require('dotenv');
+require('dotenv').config()
 const port = process.env.PORT || 5000
 
-require('dotenv').config()
+const app = express()
 
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h9ef9oj.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run () {
+try{
+const categoriesCollection = client.db('crazy-resale-deals').collection('categories')
+
+// categories
+app.get('/categories', async(req, res) => {
+  const query = {}
+  const result = await categoriesCollection.find(query).toArray();
+  res.send(result) 
+})
+
+}
+finally{}
+}
+run().catch(console.log())
 app.use(cors())
 app.use(express.json())
 
