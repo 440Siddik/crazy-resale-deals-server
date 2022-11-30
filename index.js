b6c6cf891d0c5c8155094877333685a0e1cbf1d9
Afraaz <ahmedalif102@gmail.com>
2022-11-30 19:04:40 +0600

    bookings get api

--- index.js
+++ index.js
@@ -42,6 +42,12 @@ async function run() {
       res.send(products);
     });
 // bookings 
+app.get('/bookings', async(req, res) => {
+  const email = req.query.email;
+  const query = {email:email}
+  const bookings = await bookingsCollection.find(query).toArray()
+  res.send(bookings)
+})
 app.post('/bookings', async (req, res) => {
   const booking = req.body;
   console.log(booking)
